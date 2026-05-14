0c8f23a28552cedd56ed9224b68d50931114421d
Gsgyedw <gohyanweipersonal@gmail.com>
2026-05-14 14:36:50 +0800

    add seqstructurePT3.py

---
+++ week3/SequenceStructurePT3.py
@@ -0,0 +1,16 @@
+# compound conditions:
+
+# and operator:
+x = 10
+if x > 0 and x < 20: # output will be True (checks if x is greater than 0 and less than 20)
+    print("x is a positive number less than 20")
+
+# or operator:
+y = 5
+if y < 0 or y > 10: # output will be False (checks if y is less than 0 or greater than 10)
+    print("y is a negative number or greater than 10")
+
+# not operator:
+z = 15
+if not z < 10: # output will be True (checks if z is not less than 10)
+    print("z is not a single-digit number")
