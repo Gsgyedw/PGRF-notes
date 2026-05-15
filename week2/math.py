d619a69775b86ce41d1b818d6fdf819b4f085ed6
Gsgyedw <gohyanweipersonal@gmail.com>
2026-05-15 12:05:05 +0800

    add math.py

---
+++ week2/math.py
@@ -0,0 +1,12 @@
+import math # math functions:
+
+print(math.sqrt(16)) # square root 
+print(math.pow(2, 3)) # power function: 2 raised to the power of 3
+print(math.pi) # value of pi
+
+# Other math functions:
+print(math.factorial(5)) # factorial of 5, (5 * 4 * 3 * 2 * 1)
+print(math.ceil(3.7)) # round up to the nearest integer, returns 4
+print(math.exp(1)) # exponential function: e raised to the power of 1
+print(math.trunc(3.7)) # truncate the decimal part, returns 3
+print(math.e) # Euler's number, the base of natural logarithms
