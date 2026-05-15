3fa080042e5510cbc05292c8361b38d05bf82887
Gsgyedw <gohyanweipersonal@gmail.com>
2026-05-15 11:54:52 +0800

    add ProgrammingInteractivity.py

---
+++ week2/ProgrammingInteractivity.py
@@ -0,0 +1,21 @@
+# programming interactivity:
+
+# get input from the user:
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+# convert input to a number:
+age = int(input("How old are you? ")) 
+print(age)
+
+# convert input to a float:
+height = float(input("How tall are you in meters? "))
+print(height)
+
+# by default, user input is a string. 
+'''to do addition of two numbers:
+you require to convert the input to input/float'''
+# example:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = num1 + num2
